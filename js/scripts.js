cb2e1311819c4f00f9e78f348e3638ef91cab56d
nathan-barrett <demeules.barrett@gmail.com>
2018-01-05 09:53:25 -0800

    add first function

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const secInYear = 3.154e+7
+
+
+
+export class Age {
+  contructor(){
+    this.earthAge = earthAge;
+    this.currentDate = currentDate;
+  }
+
+
+  function ageToSeconds(earthAge){
+      const secondsAge = earthAge * secInYear;
+      return secondsAge;
+      }
+  dateToSeconds(currentDate){
+
+  }
+}
