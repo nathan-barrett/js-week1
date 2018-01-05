499bf5cbd4b2a56b71f928f1d7f9522a55648ee4
nathan-barrett <demeules.barrett@gmail.com>
2018-01-05 09:08:46 -0800

    add bower and bower-files

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,40 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+
+//linter to run on all files in js folder//
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+//concatenating all -interface.js and placing them in tmp folder//
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+//making all files ready for the browser//
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+  .transform(babelify.configure({
+   presets: ["es2015"]
+  }))
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
+});
+//minifying scripts, Browserify must run first - then place in build folder//
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+//use bower to concat and minify all vendor js files//
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
