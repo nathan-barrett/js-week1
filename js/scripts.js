710d05ef9fe6ee5f6ee1fc74f57bca6ab2dc0932
nathan-barrett <demeules.barrett@gmail.com>
2018-01-05 10:41:55 -0800

    figured out to receive data types from moment.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 const secInYear = 3.154e+7
+const secInMonth = 2.628e+6;
+const secInDay = 86400;
 
+export class currentTime {
+  contructor() {
+    this.currentSeconds = seconds;
+    this.miuntes = minutes;
+    this.month = month;
+    this.year = year;
 
+  }
+}
 
-export class Age {
+export class userAge {
   contructor(){
-    this.earthAge = earthAge;
-    this.currentDate = currentDate;
+    this.userDay = day;
+    this.userMonth = month;
+    this.userYear = year:
   }
 
-
-  function ageToSeconds(earthAge){
-      const secondsAge = earthAge * secInYear;
-      return secondsAge;
+  function ageToSeconds(day, month, year){
+      const dayAge = day * secInday;
+      const monthAge = month * secInMonth;
+      const yearAge = year * secInYear;
+      return dayAge + monthAge + yearAge;
       }
   dateToSeconds(currentDate){
 
